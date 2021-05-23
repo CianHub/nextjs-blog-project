4dfe7e90f44ea6f032d1ca42331cf09bcf10e641
Cian <23337360+CianHub@users.noreply.github.com>
2021-05-23 14:16:26 +0100

    add image and refactor styles for hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Image from 'next/Image';
 import classes from '../styles/hero.module.css';
 export const Hero = () => {
   return (
     <section className={classes.hero}>
-      <div className={classes.image}></div>
-      <h1>Hi, I'm Cian</h1>
-      <p>This is a blog made with Next.js for fun.</p>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/author.png"
+          alt="An image showing Grim Reaper"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm the Grim Reaper</h1>
+      <p>This is a blog made with Next.js.</p>
     </section>
   );
 };
