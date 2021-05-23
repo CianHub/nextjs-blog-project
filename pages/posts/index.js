647a3d631ab681ad6c7c211c978175e69b2b05dc
Cian <23337360+CianHub@users.noreply.github.com>
2021-05-23 15:36:05 +0100

    fix bug in posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import { AllPosts } from './all-posts';
 
-export const Posts = () => {
-  return <div></div>;
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started With Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React framework, that makes full-stack React a breeze.',
+    date: '2022/04/08',
+  },
+  {
+    slug: 'getting-started-with-nextjs-2',
+    title: 'Getting Started With Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React framework, that makes full-stack React a breeze.',
+    date: '2022/02/08',
+  },
+  {
+    slug: 'getting-started-with-nextjs-3',
+    title: 'Getting Started With Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React framework, that makes full-stack React a breeze.',
+    date: '2022/05/08',
+  },
+];
+
+const Posts = () => {
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
+
+export default Posts;
