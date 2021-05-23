844c8bd45cce4156dcff94f8d5558f069f00009a
Cian <23337360+CianHub@users.noreply.github.com>
2021-05-23 14:26:26 +0100

    add contact to nav

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -1,5 +1,23 @@
 import React from 'react';
-
+import { Logo } from './Logo';
+import Link from 'next/link';
+import classes from '../../styles/main-navigation.module.css';
 export const Nav = () => {
-  return <div></div>;
+  return (
+    <header>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <li>
+          <Link href="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </nav>
+    </header>
+  );
 };
