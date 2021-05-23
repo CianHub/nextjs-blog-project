a663eeb8cfecc4aa32ee452988fb5c2752dbd3a6
Cian <23337360+CianHub@users.noreply.github.com>
2021-05-23 13:59:26 +0100

    add scaffolidng for home page components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
 };
 
 export default HomePage;
