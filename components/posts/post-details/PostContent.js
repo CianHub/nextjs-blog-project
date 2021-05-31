e62d395256263316bf851656702eac70f1fe064c
Cian <23337360+CianHub@users.noreply.github.com>
2021-05-31 20:53:56 +0100

    add ynamic rendering of markdown and contact form

--- components/posts/post-details/PostContent.js
+++ components/posts/post-details/PostContent.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import classes from '../../../styles/post-content.module.css';
 import { PostHeader } from './PostHeader';
-import ReactMarkdown from 'react-markdown';
 
 export const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.data.image}`;
 
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1];
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.data.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
