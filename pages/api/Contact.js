174d119ad0dbffcd5ee2d4dd055ff3be034a375d
Cian <23337360+CianHub@users.noreply.github.com>
2021-05-31 21:06:01 +0100

    add api contact route

---
+++ pages/api/Contact.js
@@ -0,0 +1,25 @@
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    ) {
+      res.status(422).json({ message: 'invalid input.' });
+      return;
+    }
+
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+  }
+}
+
+export default handler;
