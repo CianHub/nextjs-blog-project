2ab6ec645825dd0c0d9c6a712a4f4c27f42c550d
Cian <23337360+CianHub@users.noreply.github.com>
2021-06-05 17:11:51 +0100

    fix typo;

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/Image';
+import Image from 'next/image';
 import classes from '../styles/hero.module.css';
 export const Hero = () => {
   return (
